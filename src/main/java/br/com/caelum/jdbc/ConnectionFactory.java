6ea859d24e1c098dcf4779fa8dbff8754700d5eb
MiKalec <msmikaelis@gmail.com>
2018-08-14 14:29:14 -0300

    Update ConnectionFactory.java

--- src/main/java/br/com/caelum/jdbc/ConnectionFactory.java
+++ src/main/java/br/com/caelum/jdbc/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection getConnection(){
         try{
-            return DriverManager.getConnection("jdbc:mysql://localhost/jdbc", "root", "Mik@k@1234");
+            return DriverManager.getConnection("jdbc:mysql://localhost/jdbc", "root", "senha");
         }catch (SQLException e){
             throw new RuntimeException(e);
         }
